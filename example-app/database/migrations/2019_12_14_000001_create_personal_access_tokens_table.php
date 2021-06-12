8d292ee328b12e98090f26827d9033c9bc4d4bb2
hoanghuulong <hoanghuulong.krn@gamil.com>
2021-06-12 09:28:20 +0700

    Installing Jetstream

--- example-app/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ example-app/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,7 +15,9 @@ class CreatePersonalAccessTokensTable extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->morphs('tokenable');
+            //$table->morphs('tokenable');
+            $table->unsignedInteger("taggable_id");
+            $table->string("taggable_type", 100);
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
